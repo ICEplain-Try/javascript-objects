c4c36df382e5de6dc5e2b5340236d5d48028ea01
ICE K. <info.icejourney@gmail.com>
2024-08-13 14:07:23 +0700

    update: ex_1-3

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,19 @@
 // Exercise #1: Food Order
 
 // Start coding here
+let foodOrder = {
+    orderNumber: "A0234",
+    address: "Bangkok",
+    paymentType: "Cash",
+    totalPrice: 4500,
+    restauranName: "MK",
+};
+
+// ลองดูผลลัพธ์ก่อน
+console.log (foodOrder);
+
+// Reassign
+foodOrder.totalPrice = 5000;
+foodOrder.paymentType = "Credit Card";
+
+console.log (foodOrder);
--- ex-2.js
+++ ex-2.js
@@ -7,4 +7,20 @@ const product = {
   photo: null,
 };
 let socialMediaShare = "facebookShare";
+
+
 // Start code here
+console.log (product);
+
+// Reassignment
+product.ratings = 4.5;
+product.photo = "https://i01.appmifile.com/webfile/globalimg/products/pc/mi-air-purifier-3H/replace_03.jpg";
+
+product.code = "IT0101010";
+product.price = 6000;
+
+delete product.ratings;
+
+product[socialMediaShare] = 45.5;
+
+console.log(product);
